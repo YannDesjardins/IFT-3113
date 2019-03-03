79586074e082aee1f162f7d6da61b55b5ecb0dd2
Yann_IFT <yann.benard-desjardins.1@ulaval.ca>
2019-03-02 19:41:07 -0500

    Proto

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool GamePaused = false;
+	public GameObject pauseMenuUI;
+	public Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (GamePaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+    }
+	public void Resume(){
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		mainCamera.GetComponent<FlyCamera>().enabled = true;
+		Cursor.visible = false;
+		GamePaused = false;
+	}
+	void Pause(){
+		pauseMenuUI.SetActive(true);
+		mainCamera.GetComponent<FlyCamera>().enabled = false;
+		Cursor.visible = true;
+		Time.timeScale = 0f;
+		GamePaused = true;
+
+	}
+	public void Quit(){
+		Application.Quit();
+
+	}
+}
