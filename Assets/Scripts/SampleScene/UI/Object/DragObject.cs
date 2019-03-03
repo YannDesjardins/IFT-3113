23d6d90949eb8882e47d1fb8879fb6a0034a253c
dupeg <guillaume.dupe@epitech.eu>
2019-03-03 10:58:36 -0500

    Repo light

---
+++ Assets/Scripts/SampleScene/UI/Object/DragObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragObject : MonoBehaviour
+{
+    Canvas  topParent;
+    public Slot slot;
+    Vector3 posInSlot;
+    Image img;
+
+    public void Start()
+    {
+        img = GetComponent<Image>();
+        img.raycastTarget = true;
+    }
+}
---
+++ Assets/Scripts/SampleScene/UI/Object/Slot.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    Object                      obj = new Object();
+    Image                       img;
+    Text                        quantity;
+
+    public int              uid = -1;
+
+    public void Start()
+    {
+        img = transform.GetComponentInChildren<DragObject>(true).gameObject.GetComponent<Image>();
+        quantity = img.gameObject.GetComponentInChildren<Text>(true);
+        Assign(obj);
+    }
+
+    public bool Assign(Object newObj)
+    {
+        KeyValuePair<Sprite, AObject> itemData;
+
+        if (newObj.id == -1)
+            return true;
+        itemData = Manager.GetItem(newObj);
+        obj.categorie = newObj.categorie;
+        obj.id = newObj.id;
+        obj.quantity = newObj.quantity;
+        img.gameObject.SetActive(obj.id != -1);
+        img.overrideSprite = itemData.Key;
+        quantity.text = obj.quantity.ToString();
+        quantity.gameObject.SetActive(obj.quantity > 1);
+
+        return true;
+    }
+
+    public void Clear()
+    {
+        obj.id = -1;
+        obj.quantity = 1;
+        img.gameObject.SetActive(false);
+        img.overrideSprite = null;
+    }
+}
