d5a7b550252f0ea64ff3aee3038c9ca7684a0390
Marc Lauzon <kalahee@gmail.com>
2019-03-19 22:51:52 -0400

    Surfacing Delay Timer
    Ajout du compteur et gestion du compteur au-dessus d'une hauteur.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,62 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using TMPro;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+namespace Thalass {
+    public class GameManager : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.Escape))
-            ToggleCursor();
-    }
+        [SerializeField]
+        SubmarineController m_submarine = null;
 
-    void ToggleCursor() {
-        Cursor.lockState = (Cursor.lockState == CursorLockMode.None) ? CursorLockMode.Locked : CursorLockMode.None;
+        [Header("Surfacing")]
+        [SerializeField]
+        GameObject m_timerObject = null;
+
+        [Space]
+        [SerializeField]
+        float m_exitHeight = 30.0f;
+
+        [SerializeField]
+        float m_exitTimerDelay = 5.0f;
+        float m_exitTimerLeft = 5.0f;
+        bool m_isExiting = false;
+
+        [Space]
+        [SerializeField]
+        TMP_Text m_countDown = null;
+
+        void Start() {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        void Update() {
+            /*
+            if (Input.GetKeyUp(KeyCode.Escape))
+                ToggleCursor();
+            */
+        }
+
+        void LateUpdate() {
+            if(m_submarine.transform.position.y > m_exitHeight) {
+                m_timerObject.SetActive(true);
+
+                if (m_isExiting) {
+                    m_exitTimerLeft -= Time.fixedDeltaTime;
+                    m_countDown.text = m_exitTimerLeft.ToString("F3");
+                } else {
+                    m_exitTimerLeft = m_exitTimerDelay;
+                    m_isExiting = true;
+                }
+            } else {
+                m_timerObject.SetActive(false);
+
+                if (m_isExiting)
+                    m_isExiting = false;
+            }
+        }
+
+        void ToggleCursor() {
+            Cursor.lockState = (Cursor.lockState == CursorLockMode.None) ? CursorLockMode.Locked : CursorLockMode.None;
+        }
     }
 }
